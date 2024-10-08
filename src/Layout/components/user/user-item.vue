65a6eb8c94c03938a397e5065fdd6ce9fd2003ab
ZRMYDYCG <547471919@qq.com>
2024-10-08 18:31:22 +0800

    style: 调整代码格式

--- src/Layout/components/user/user-item.vue
+++ src/Layout/components/user/user-item.vue
@@ -1,13 +1,13 @@
 <template>
+  <div>
     <div>
-        <div>
-            <img src="" alt="#">
-        </div>
-        <div>
-            <p>一勺</p>
-            <p>[在线]</p>
-        </div>
+      <img src="" alt="#" />
     </div>
+    <div>
+      <p>一勺</p>
+      <p>[在线]</p>
+    </div>
+  </div>
 </template>
 <script setup lang="ts"></script>
 <style lang="scss" scoped></style>
